123f16e1de99b0a6d9ac7b13d3608b6d755633bd
sabal62 <sabalplayz.new@gmail.com>
2026-02-13 11:09:06 +0545

    Fix API url

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 // frontend/src/services/api.js
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000/api';
+const API_BASE_URL = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
     baseURL: API_BASE_URL,
